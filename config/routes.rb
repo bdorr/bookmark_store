e82537011ee667e5618058305e817b560cec6750
Brandon Dorr <bdorr@madisoncollege.edu>
2013-10-11 15:31:59 -0500

    Views created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BookmarkStore::Application.routes.draw do
+  resources :bookmarks
+
   get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
